8e03c307ae33391c031126a2859a3b3d9901553e
abhighosh98 <abhighosh98@gmail.com>
2022-07-08 23:37:33 -0700

    feat: added details to 3_Achievements.py

--- pages/3_Achievements.py
+++ pages/3_Achievements.py
@@ -25,7 +25,11 @@ with st.container():
         st.image(Image.open('Images/convocation 2.jpeg'), width = 320)
     with col2:
         st.image(Image.open('Images/Scholarship award.jpeg'), width = 370)
-best = """"""
+best = """This is an award that I had been eyeing for about 4 years; ever 
+since I heard about it in the first year of my undergraduate degree. Awarded to 
+the best student amongst all engineering branches. This award took into account 
+not just grades but also techincal skills, social responsibilities, positions of 
+leadership and many more facets of an individual. """
 st.write(best)
 st.markdown('#')
 st.markdown('#')
@@ -34,15 +38,22 @@ st.markdown('#')
 st.markdown('<p class="biggest-font"><b>➲  1st Place in the Xavier Coding Hackathon 2019</b></p>', unsafe_allow_html=True)
 with st.container():
     st.image(Image.open('Images/Hackathon First Prize.jpeg'), width = 640)
-hackathon = """"""
+hackathon = """The Xavier Coding Hackathon was a coding competition which attracted 
+the best students from many colleges in the city. It had 3 rounds with increasingly 
+levels of difficulty and took place over the course of a day, rounding off with the 
+prize distribution. Dr. Frank Trocki, Chancellor at Montana State University 
+felicated the winners of this competiton. """
 st.write(hackathon)
 st.markdown('#')
 st.markdown('#')
 
-st.markdown('<p class="biggest-font"><b>➲  1st Place in Project Exhibition</b></p>', unsafe_allow_html=True)
+st.markdown('<p class="biggest-font"><b>➲  1st Place in Spandan Project Exhibition</b></p>', unsafe_allow_html=True)
 with st.container():
     st.image(Image.open('Images/Spandan Project Exhibition Prize.jpeg'), width = 640)
-spandan = """"""
+spandan = """Spandan is an annual college festival organized by the student council 
+at Xavier Institute of Engineering. The project exhibition was an event under the
+technical side of the festival; Transmission. The judges were industry professionals 
+from CapGemini with years of experience under their belt. """
 st.write(spandan)
 st.markdown('#')
 st.markdown('#')
@@ -64,3 +75,15 @@ go recognition for same. """
 st.write(protech)
 st.markdown('#')
 st.markdown('#')
+
+st.markdown('<p class="biggest-font"><b>➲  1st Place in Final Year Project Exhibition</b></p>', unsafe_allow_html=True)
+with st.container():
+    st.image(Image.open('Images/final year exhibition.JPG'), width = 640)
+final = """This competition was held online in July 2020 during the lockdown brought 
+about due to Covid 19. Industry experts were brought in as the panelists to judge our 
+projects. After a grueling day of presentations and demos, our team came out of top 
+amognst all the engineering branches. """
+st.write(final)
+st.markdown('#')
+st.markdown('#')
+
